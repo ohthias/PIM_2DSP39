b1a6b8269dc7a06d37677b4e34233cab8a2b004b
Matheus Gabriel <math.gsilva07@gmail.com>
2025-11-10 20:30:12 +0000

    Adiciona importação do Blueprint em app.py e cria arquivo requirements.txt com dependências do projeto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
 from ai_module.recommendation import recomendar_materiais
